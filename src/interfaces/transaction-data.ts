2a365a3b7f5168f303e8ff5fc5fe6161551e5377
SKitttz <skittz.dev@gmail.com>
2024-11-14 20:28:43 -0300

    feat(interfaces): create transaction interfaces

---
+++ src/interfaces/transaction-data.ts
@@ -0,0 +1,41 @@
+import { ReactNode } from "react";
+
+export interface ITransaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
+export interface ICreateTransaction {
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+}
+
+export interface ITransactionSummary {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+
+export interface ITransactionContextType {
+  transactions: ITransaction[];
+  loadTransactions: (query?:string) => Promise<void>;
+  createTransaction: (transaction:ICreateTransaction) => Promise<void>;
+  summaryTransactions: ITransactionSummary;
+}
+
+export interface ITransactionProviderProps {
+  children: ReactNode;
+}
+
+export interface IReducerTransaction {
+  income: number;
+  outcome: number;
+  total: number;
+}
